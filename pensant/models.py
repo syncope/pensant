f19c3985f570be5b864a51bd3fdebfa2f4c35280
Christoph Rosemann <christoph.rosemann@desy.de>
2018-04-13 10:10:02 +0200

    + first working version for manual guessing of gaussian fit start parameters via slider setting done

--- pensant/models.py
+++ pensant/models.py
@@ -28,6 +28,7 @@ from PyQt4 import QtCore, QtGui, uic
 import numpy as np
 
 class ParameterSettingDialog(QtGui.QDialog):
+    updateFit = QtCore.pyqtSignal()
 
     def __init__(self, uifile, parent=None):
         super(ParameterSettingDialog, self).__init__(parent)
@@ -37,36 +38,74 @@ class ParameterSettingDialog(QtGui.QDialog):
         self._xdata = xdata
         self._ydata = ydata
         self.update()
-    
+
     def update(self):
         pass
 
 class GaussianParameterSettingDialog(ParameterSettingDialog):
     
-    def __init__(self, xdata, ydata, **kw):
+
+    def __init__(self, xdata, ydata, model=None, **kw):
         super(GaussianParameterSettingDialog, self).__init__(**kw)
         self.passData(xdata, ydata)
+        self.meanSlider.valueChanged.connect(self._meanScaler)
+        self.amplitudeSlider.valueChanged.connect(self._ampScaler)
+        self.sigmaSlider.valueChanged.connect(self._sigmaScaler)
+        self._model = model
 
     def update(self):
-        self._meanDisplay = ( float(np.mean(self._xdata)), float(np.amin(self._xdata)), float(np.amax(self._xdata)))
-        self._amplitudeDisplay =  ( float(np.mean(self._ydata)), float(np.amin(self._ydata)), float(np.amax(self._ydata))) 
-        self._sigmaDisplay = (float(self._meanDisplay[2] - self._meanDisplay[1])/3.5)
-        self.meanValue.setValue(self._meanDisplay[0])
-        self.meanLBValue.setValue(self._meanDisplay[1])
-        self.meanUBValue.setValue(self._meanDisplay[2])
-
-        self.amplitudeValue.setValue(self._amplitudeDisplay[0])
-        self.amplitudeLBValue.setValue(self._amplitudeDisplay[1])
-        self.amplitudeUBValue.setValue(self._amplitudeDisplay[2])
-
+        # first basic calculations 
+        self._meanDisplay = float(np.mean(self._xdata))
+        self._meanBounds = (float(np.amin(self._xdata)), float(np.amax(self._xdata)))
+        self._sigmaDisplay = float(np.amax(self._xdata) - np.amin(self._xdata))/5.
+        self._amplitudeDisplay = float(np.amax(self._ydata))/5.
+        # now set initial values
+        self.meanValue.setValue(self._meanDisplay)
+        self.amplitudeValue.setValue(self._amplitudeDisplay)
         self.sigmaValue.setValue(self._sigmaDisplay)
 
-def modelWidgeteer(model, uiFilename, xdata, ydata):
+        # and now the boundaries -- as valid for the slider
+        # mean:
+        self.meanLBValue.setValue(float(np.amin(self._xdata)))
+        self.meanUBValue.setValue(float(np.amax(self._xdata)))
+        # amplitude
+        self.amplitudeLBValue.setValue(float(np.amin(self._ydata)))
+        self.amplitudeUBValue.setValue(float(np.amax(self._ydata)))
+        # sigma
+        self.sigmaLBValue.setValue(self._sigmaDisplay/5.)
+        self.sigmaUBValue.setValue(self._sigmaDisplay*5)
+        self.updateFit.emit()
+
+    def _meanScaler(self, val):
+        valuewidth = self._meanBounds[1]-self._meanBounds[0]
+        currentVal = (self.meanSlider.minimum() + val * self.meanSlider.singleStep())/(self.meanSlider.maximum() - self.meanSlider.minimum())
+        self.meanValue.setValue(self._meanBounds[0] + currentVal*valuewidth)
+        self.updateFit.emit()
+
+    def _ampScaler(self, val):
+        valuewidth = self.amplitudeUBValue.value() - self.amplitudeLBValue.value()
+        currentVal = (self.amplitudeSlider.minimum() + val * self.amplitudeSlider.singleStep())/(self.amplitudeSlider.maximum() - self.amplitudeSlider.minimum())
+        self.amplitudeValue.setValue(self.amplitudeLBValue.value() + currentVal*valuewidth)
+        self.updateFit.emit()
+
+    def _sigmaScaler(self, val):
+        valuewidth = self.sigmaUBValue.value() - self.sigmaLBValue.value()
+        currentVal = (self.sigmaSlider.minimum() + val * self.sigmaSlider.singleStep())/(self.sigmaSlider.maximum() - self.sigmaSlider.minimum())
+        self.sigmaValue.setValue(self.sigmaLBValue.value() + currentVal*valuewidth)
+        self.updateFit.emit()
+
+    def getCurrentFitData(self):
+        params = self._model.make_params(center=self.meanValue.value(), amplitude=self.amplitudeValue.value(), sigma=self.sigmaValue.value())
+        return self._model.eval(params, x=self._xdata) 
+
+def modelWidgeteer(model, fitModel, uiFilename, xdata, ydata):
     import os
     dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
     formfile = os.path.join(dir_path, uiFilename)
     if model == "gaussian":
-        return GaussianParameterSettingDialog(xdata, ydata, uifile=formfile)
+        return GaussianParameterSettingDialog(xdata, ydata, fitModel, uifile=formfile)
+    else:
+        return ParameterSettingDialog(uifile=formfile)
 
 class gaussian(GaussianModel):
     def __init__(self, **kwargs):
@@ -74,7 +113,7 @@ class gaussian(GaussianModel):
         self._widget = None
 
     def getWidget(self, xdata=None, ydata=None):
-        self._widget = modelWidgeteer("gaussian", "ui/gaussModelFitParameters.ui", xdata, ydata)
+        self._widget = modelWidgeteer("gaussian", self, "ui/gaussModelFitParameters.ui", xdata, ydata)
         return self._widget
 
     def guess(self, data, **kw):
