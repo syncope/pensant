dbe6395457ed46e6b63d9226463880e7c0a78525
Christoph Rosemann <christoph.rosemann@desy.de>
2018-06-08 10:41:19 +0200

    + preparing for alpha release, until I find time to fix the issues

--- README.rst
+++ README.rst
@@ -1,2 +1,12 @@
-This is just an initial file for testing things out.
+PENSANT: Parameter estimation helpers
+-------------------------------------
 
+This package provides additional functionality to the lmfit package.
+lmfit :  https://dx.doi.org/10.5281/zenodo.11813
+Its main purpose is to add GUI elements by pyqt graphics.
+It also provides an additional interface to create composite models with automatic naming schemes.
+
+The package is for Python3.
+
+The most simple way is to clone the repository and expand your (PYTHON)PATH variable to the base directory.
+A Debian package is also available.
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+with open(path.join('.', 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+name='pensant'
+version='0'
+release='0.0.3'
+
+setup(
+    name='pensant',
+    version='0.0.3',
+
+    description='Library to simplify handling parameter estimation, also providing GUI elements within pyqt.', 
+    long_description=long_description,
+
+    url='https://github.com/syncope/pensant',
+
+    author='Ch.Rosemann',
+    author_email='christoph.rosemann@desy.de',
+    
+    license='GPLv2',
+    
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='pensant parameter estimation lmfit',
+    
+    packages=['pensant',],
+    
+    package_dir = { 'pensant':'pensant',},
+    
+    include_package_data=True,
+    
+)
+
